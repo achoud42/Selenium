1a35dd9d80410c285fcce63a5ab6175605542ce0
achoud42 <118777662+achoud42@users.noreply.github.com>
2023-01-31 21:57:34 +0530

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,9 @@
+#this is for constants we are using in our vintest.py
+cookies = '//*[@id="onetrust-accept-btn-handler"]'
+randomVinUrl = 'https://randomvin.com/getvin.php?type=real'
+carfaxUrl = 'https://www.carfax.eu/'
+vinSearchXpath = 'vin-search-default'
+nextArrowXpath =  '//*[@id="__next"]/div/div/main/div/div[1]/div[1]/div/div/div/div/div/form/button/div'
+recordsXpath = '//*[@id="__next"]/div/div/main/div/div[1]/div[2]/h1'
+
+
